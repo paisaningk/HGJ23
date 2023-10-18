6becf851de6d863268cb241526c0c3a3904dcfe8
paisaningk <58562404+paisaningk@users.noreply.github.com>
2023-10-18 16:40:43 +0700

    add mainmenu

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public Button startGame;
+        public Button exitGame;
+
+        public void Start()
+        {
+            startGame.onClick.AddListener(StarGame);
+            exitGame.onClick.AddListener(ExitGame);
+        }
+
+        public void StarGame()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public void ExitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
